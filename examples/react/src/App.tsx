c2b5443949146f274b9af8102ce534e998c27d26
NorbertMerkli <norbertmerkli11@gmail.com>
2025-07-27 14:26:28 +0200

    Adjust example layout

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,7 +1,8 @@
+import type { CSSProperties } from "react";
 import {
   DerivedSignal,
   Signal,
-  useSignal,
+  useSetSignal,
   useSignalValue,
   type ReadonlySignal,
 } from "signal-react";
@@ -12,23 +13,55 @@ const c = new DerivedSignal((get) => get(a) + get(b));
 
 export const App = () => {
   return (
-    <>
-      <Controller signal={a} />
-      <Controller signal={b} />
+    <div
+      style={{
+        fontSize: "2rem",
+        fontWeight: "bold",
+        display: "grid",
+        gridTemplateColumns: "1fr auto 1fr auto 1fr",
+        gridTemplateRows: "1fr 1fr",
+        justifyItems: "center",
+        gap: "1rem",
+      }}
+    >
+      <Value signal={a} />
+      <span>+</span>
+      <Value signal={b} />
+      <span>=</span>
       <Value signal={c} />
-    </>
+      <Controller signal={a} />
+      <Controller signal={b} style={{ gridColumnStart: 3 }} />
+    </div>
   );
 };
 
-const Controller = (props: { signal: Signal<number> }) => {
-  const [state, setState] = useSignal(props.signal);
+const Controller = (props: {
+  signal: Signal<number>;
+  style?: CSSProperties;
+}) => {
+  const setState = useSetSignal(props.signal);
 
   return (
-    <span>
-      <span>{state}</span>
-      <button onClick={() => setState((state) => --state)}>-</button>
-      <button onClick={() => setState((state) => ++state)}>+</button>
-    </span>
+    <div
+      style={{
+        display: "flex",
+        gap: "0.5rem",
+        ...props.style,
+      }}
+    >
+      <button
+        style={{ paddingInline: "1rem" }}
+        onClick={() => setState((state) => --state)}
+      >
+        -
+      </button>
+      <button
+        style={{ paddingInline: "1rem" }}
+        onClick={() => setState((state) => ++state)}
+      >
+        +
+      </button>
+    </div>
   );
 };
 
